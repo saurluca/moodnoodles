300d6746e34f5ac968ca7f0dc026d3d052ab5f7f
saurluca <spuckteller@gmail.com>
2025-01-12 16:14:35 +0100

    added nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    modules: [
-        '@vueuse/nuxt',
-        '@nuxtjs/supabase',
-    ],
+    modules: ['@vueuse/nuxt', '@nuxtjs/supabase', '@nuxthub/core'],
     devtools: {enabled: true},
     css: ['~/assets/css/main.css'],
     ssr: false,
